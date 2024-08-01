bdd474e8ca0c7c166822560fa8ef395bf0463d36
Carlos Rodriguez <imindloop@gmail.com>
2024-08-01 08:30:57 -0600

    Cambiamos el estudiante a Celina

--- src/App.js
+++ src/App.js
@@ -15,8 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Carlos is learning React!
-        </a>
+          Ahora Celina va a aprender React!
       </header>
     </div>
   );
